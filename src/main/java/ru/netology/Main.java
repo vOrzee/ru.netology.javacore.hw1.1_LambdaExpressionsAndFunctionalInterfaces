310427ef963d8a7438d5e252f22ae77c34bc1260
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-04-28 18:59:44 +0500

    Initial condition

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,6 +2,11 @@ package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
     }
 }
