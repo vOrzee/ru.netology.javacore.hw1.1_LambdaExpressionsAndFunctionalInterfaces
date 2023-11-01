392c57802573ce743e23122535c656a00ae2722a
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-11-01 18:22:04 +0500

    Fix

--- src/main/java/ru/netology/Calculator.java
+++ src/main/java/ru/netology/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0; //но очень хочется null вернуть... 0 всё-таки не специфическое значение, 0/5 = 0
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,14 +6,12 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
+        int b = calc.minus.apply(1, 1);
 
-        try {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Infinite (division by zero)");
-        }
+
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+
         // Возникает ошибка от деления на 0 в целых числах, отлавливаем её и выводим сообщение
         // Как второй вариант можно перевести devide на Double, тогда исключение выброшено не будет и результат для остальных чисел будет более точным
         // Однако также придётся перевести на Double и Consumer println
